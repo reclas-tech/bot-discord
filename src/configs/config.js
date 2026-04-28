216ab3a1374882be5021414f110b1b89dd29f01e
randaru09 <Ndaruanggoro123@gmail.com>
2026-04-28 14:36:44 +0700

    feat(config): add YouTube channel and announcement configuration variables

--- src/configs/config.js
+++ src/configs/config.js
@@ -3,4 +3,8 @@ require("dotenv").config();
 module.exports = {
     botToken: process.env.DISCORD_BOT_TOKEN,
     welcomeChannelId: process.env.DISCORD_WELCOME_CHANNEL_ID,
+    youtubeChannelId: process.env.YOUTUBE_CHANNEL_ID,
+    youtubeApiKey: process.env.YOUTUBE_API_KEY,
+    youtubeUploadAnnouncementChannelId: process.env.DISCORD_YOUTUBE_UPLOAD_ANNOUNCEMENT_CHANNEL_ID,
+    youtubeLiveAnnouncementChannelId: process.env.DISCORD_YOUTUBE_LIVE_ANNOUNCEMENT_CHANNEL_ID,
 };
